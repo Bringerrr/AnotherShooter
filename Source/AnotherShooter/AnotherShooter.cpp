f1b207c593183370f6b5397566ff998f8e9f2074
Bringerr <Kruchkovevg21@gmail.com>
2023-07-12 00:01:31 +0300

    first commit

---
+++ Source/AnotherShooter/AnotherShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AnotherShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AnotherShooter, "AnotherShooter" );
---
+++ Source/AnotherShooter/AnotherShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnotherShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANOTHERSHOOTER_API AAnotherShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
